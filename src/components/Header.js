e46ba3c1aea7e2cb2769a162be6728091e3cee7c
jimin song <songing01@gmail.com>
2023-11-03 04:10:37 +0900

    💄 공용헤더 버튼 추가

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,27 +1,67 @@
 import React from "react";
 import { styled } from "styled-components";
-import logo from "../assets/login/logo.png";
-const Header = ({ title }) => {
+import logo from "../assets/common/logo.png";
+import alarm from "../assets/common/alarm.png";
+import user from "../assets/common/user.png";
+const Header = ({ title, isLogin = false }) => {
   return (
     <Wrapper>
-      <img className="logo" src={logo} alt="Let's JUPJUP" />
-      <div className="title">{title}</div>
+      <div className="left">
+        <img className="logo" src={logo} alt="Let's JUPJUP" />
+        <div className="title">{title}</div>
+      </div>
+
+      {isLogin && (
+        <div className="btns">
+          <div className="btn">
+            <img src={alarm} alt="알림모음으로" />
+          </div>
+          <div className="btn">
+            <img src={user} alt="마이페이지로" />
+          </div>
+        </div>
+      )}
     </Wrapper>
   );
 };
 
 export default Header;
 const Wrapper = styled.div`
+  height: 48px;
   display: flex;
-  align-items: end;
-  .logo {
-    margin-left: 24px;
-    width: 151.386px;
-    height: 48px;
-    flex-shrink: 0;
+  justify-content: space-between;
+  .left {
+    display: flex;
+    align-items: end;
+
+    .logo {
+      margin-left: 24px;
+      width: 151.386px;
+      height: 48px;
+      flex-shrink: 0;
+    }
+
+    .title {
+      margin-left: 8px;
+    }
   }
 
-  .title {
-    margin-left: 8px;
+  .btns {
+    display: flex;
+    flex-direction: column;
+    height: 100%;
+    justify-content: space-between;
+
+    margin-right: 16px;
+    .btn {
+      width: 20px;
+      height: 20px;
+      display: flex;
+
+      img {
+        width: 20px;
+        height: 20px;
+      }
+    }
   }
 `;
