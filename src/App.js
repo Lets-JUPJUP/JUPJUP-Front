81ab4f1055d483544df50142241cb85a71a8df5b
wowalswjd <alswjd5491@naver.com>
2023-11-04 01:36:51 +0900

    :art: App.js에 플로깅 리스트 페이지 route 추가

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import { Routes, Route } from "react-router-dom";
+import PloggingListPage from "./pages/PloggingListPage";
 
 function App() {
-  return <div className="App">Lets-JUPJUP</div>;
+  return (
+    <Routes>
+      <Route path="/plogging-list" element={<PloggingListPage />} />
+    </Routes>
+  );
 }
 
 export default App;
