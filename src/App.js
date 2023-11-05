cf305837b2cc3140ce6200259cace6ba69f84665
wowalswjd <alswjd5491@naver.com>
2023-11-06 04:21:49 +0900

    :truck: PloggingListPage 파일 Plogging 폴더로 이동

--- src/App.js
+++ src/App.js
@@ -2,7 +2,8 @@ import { Routes, Route } from "react-router-dom";
 import LoginPage from "./pages/Login/LoginPage";
 import LoginSettingsPage from "./pages/Login/LoginSettingsPage";
 import CreatePlogPage from "./pages/Plogging/CreatePlogPage";
-import PloggingListPage from "./pages/PloggingListPage";
+import PloggingListPage from "./pages/Plogging/PloggingListPage";
+import PloggingDetailPage from "./pages/Plogging/PloggingDetailPage";
 import UserReportPage from "./pages/User/UserReportPage";
 
 function App() {
@@ -12,6 +13,7 @@ function App() {
       <Route path="login-settings" element={<LoginSettingsPage />} />
       <Route path="create-plogging" element={<CreatePlogPage />} />
       <Route path="/plogging-list" element={<PloggingListPage />} />
+      <Route path="/plogging-detail/:id" element={<PloggingDetailPage />} />
       <Route path="user-report" element={<UserReportPage />} />
     </Routes>
   );
