ce82fa88466c79530f468e09c14deff3423e8ee8
wowalswjd <alswjd5491@naver.com>
2023-11-07 05:42:19 +0900

    :lipstick: Header props 추가, 가운데 정렬 수정

--- src/components/common/Header.js
+++ src/components/common/Header.js
@@ -3,33 +3,48 @@ import { styled } from "styled-components";
 import logo from "../../assets/common/logo.png";
 import alarm from "../../assets/common/alarm.png";
 import user from "../../assets/common/user.png";
-const Header = ({ title, isLogin = false }) => {
-  return (
-    <Wrapper>
-      <div className="left">
-        <img className="logo" src={logo} alt="Let's JUPJUP" />
-        <div className="title">{title}</div>
-      </div>
+import GradientLine from "./GradientLine";
 
-      {isLogin && (
-        <div className="btns">
-          <div className="btn">
-            <img src={alarm} alt="알림모음으로" />
+const Header = ({ title, isLogin = false, isDetailPage = false }) => {
+  return (
+    <>
+      <Wrapper>
+        {isDetailPage === true ? (
+          <div className="detailTitle">{title}</div>
+        ) : (
+          <div className="left">
+            <img className="logo" src={logo} alt="Let's JUPJUP" />
+            <div className="title">{title}</div>
           </div>
-          <div className="btn">
-            <img src={user} alt="마이페이지로" />
+        )}
+
+        {isLogin && (
+          <div className="btns">
+            <div className="btn">
+              <img src={alarm} alt="알림모음으로" />
+            </div>
+            <div className="btn">
+              <img src={user} alt="마이페이지로" />
+            </div>
           </div>
-        </div>
-      )}
-    </Wrapper>
+        )}
+      </Wrapper>
+      {isDetailPage === true ? <GradientLine /> : null}
+    </>
   );
 };
 
 export default Header;
+
 const Wrapper = styled.div`
-  height: 57px;
+  width: 100%;
+  /* height: 57px; */
+  height: 70px;
   display: flex;
   justify-content: space-between;
+  align-items: center;
+  background: var(--white, #fff);
+
   .left {
     height: 48px;
     display: flex;
@@ -50,8 +65,14 @@ const Wrapper = styled.div`
     }
   }
 
+  .detailTitle {
+    font-size: 16px;
+    font-weight: 600;
+    margin-left: 24px;
+  }
+
   .btns {
-    margin-top: 9px;
+    /* margin-top: 9px; */
     display: flex;
     flex-direction: column;
     height: 48px;
