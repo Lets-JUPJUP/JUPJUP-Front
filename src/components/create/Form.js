402dacf96a24182bea83d3b7cffd908a623e7af2
jimin song <songing01@gmail.com>
2023-11-13 16:27:26 +0900

    💄 날짜 입력란 디자인 수정

--- src/components/create/Form.js
+++ src/components/create/Form.js
@@ -5,6 +5,39 @@ import { age_marks, count_marks } from "./marks";
 import add from "../../assets/create/add.png";
 import AddPhoto from "../common/AddPhoto";
 const Form = () => {
+  const [startDate, setStartDate] = useState({
+    month: "",
+    date: "",
+    hour: "",
+    min: "",
+  });
+  const [joinDate, setJoinDate] = useState({
+    month: "",
+    date: "",
+    hour: "",
+    min: "",
+  });
+
+  const handleStartDateChange = (e) => {
+    const { value, name } = e.target;
+    setStartDate({
+      ...startDate,
+      [name]: String(value).padStart(2, "0"),
+    });
+
+    console.log(startDate);
+  };
+
+  const handleJoinDateChange = (e) => {
+    const { value, name } = e.target;
+    setJoinDate({
+      ...joinDate,
+      [name]: String(value).padStart(2, "0"),
+    });
+
+    console.log(startDate);
+  };
+
   return (
     <Wrapper>
       <div className="gradient" />
@@ -12,18 +45,76 @@ const Form = () => {
         <div className="title">제목</div>
         <Input placeholder="입력하기" />
         <Divider />
-
         <div className="subjects">출발 일시</div>
-        <Input placeholder="입력하기" />
+        <div className="start-inputs">
+          <input
+            className="month"
+            name="month"
+            placeholder="00"
+            type="number"
+            min="1"
+            max="12"
+            onChange={handleStartDateChange}
+            maxLength="2"
+            onInput={(e) => {
+              if (e.target.value.length > e.target.maxLength)
+                e.target.value = e.target.value.slice(0, e.target.maxLength);
+            }}
+          />
+          월
+          <input
+            className="date"
+            name="date"
+            placeholder="00"
+            type="number"
+            min="1"
+            max="31"
+            onChange={handleStartDateChange}
+            maxLength="2"
+            onInput={(e) => {
+              if (e.target.value.length > e.target.maxLength)
+                e.target.value = e.target.value.slice(0, e.target.maxLength);
+            }}
+          />
+          일
+          <input
+            className="hour"
+            name="hour"
+            placeholder="00"
+            type="number"
+            min="0"
+            max="23"
+            onChange={handleStartDateChange}
+            maxLength="2"
+            onInput={(e) => {
+              if (e.target.value.length > e.target.maxLength)
+                e.target.value = e.target.value.slice(0, e.target.maxLength);
+            }}
+          />
+          시
+          <input
+            className="min"
+            name="min"
+            placeholder="00"
+            type="number"
+            min="0"
+            max="60"
+            onChange={handleStartDateChange}
+            maxLength="2"
+            onInput={(e) => {
+              if (e.target.value.length > e.target.maxLength)
+                e.target.value = e.target.value.slice(0, e.target.maxLength);
+            }}
+          />
+          분
+        </div>
 
         <div className="subjects">출발 장소</div>
         <Input placeholder="입력하기" />
-
         <div className="subjects">참여 인원</div>
         <div className="slider-container">
           <RangeSlider marks={count_marks} min={2} max={10} step={1} />
         </div>
-
         <div className="subjects">참여 성별</div>
         <ButtonContainer>
           <div className="btns">
@@ -36,24 +127,81 @@ const Form = () => {
             <div className="btn">반려동물과 함께</div>
           </div>
         </ButtonContainer>
-
         <div className="subjects">참여 연령</div>
         <div className="slider-container">
           <RangeSlider marks={age_marks} min={10} max={70} step={10} />
         </div>
-
         <Divider />
-
         <div className="subjects">본문</div>
         <Content placeholder="본문 내용을 작성해주세요." />
-
         <AddPhoto />
       </Container>
 
       <Footer>
         <div className="container">
-          <div className="date">00/00 00:00</div>
-          <div className="text">까지 모집하기</div>
+          <div className="join-inputs">
+            <input
+              className="month"
+              name="month"
+              placeholder="00"
+              type="number"
+              min="1"
+              max="12"
+              onChange={handleJoinDateChange}
+              maxLength="2"
+              onInput={(e) => {
+                if (e.target.value.length > e.target.maxLength)
+                  e.target.value = e.target.value.slice(0, e.target.maxLength);
+              }}
+            />
+            월
+            <input
+              className="date"
+              name="date"
+              placeholder="00"
+              type="number"
+              min="1"
+              max="31"
+              onChange={handleJoinDateChange}
+              maxLength="2"
+              onInput={(e) => {
+                if (e.target.value.length > e.target.maxLength)
+                  e.target.value = e.target.value.slice(0, e.target.maxLength);
+              }}
+            />
+            일
+            <input
+              className="hour"
+              name="hour"
+              placeholder="00"
+              type="number"
+              min="0"
+              max="23"
+              onChange={handleJoinDateChange}
+              maxLength="2"
+              onInput={(e) => {
+                if (e.target.value.length > e.target.maxLength)
+                  e.target.value = e.target.value.slice(0, e.target.maxLength);
+              }}
+            />
+            시
+            <input
+              className="min"
+              name="min"
+              placeholder="00"
+              type="number"
+              min="0"
+              max="60"
+              onChange={handleJoinDateChange}
+              maxLength="2"
+              onInput={(e) => {
+                if (e.target.value.length > e.target.maxLength)
+                  e.target.value = e.target.value.slice(0, e.target.maxLength);
+              }}
+            />
+            분
+          </div>
+          <div className="text">까지 모집</div>
         </div>
         <div className="submit">작성하기</div>
       </Footer>
@@ -102,6 +250,34 @@ const Wrapper = styled.div`
 const Container = styled.div`
   width: 358px;
   align-self: center;
+
+  .start-inputs {
+    display: flex;
+    gap: 4px;
+    color: var(--black, #09090a);
+    font-size: 16px;
+    font-style: normal;
+    font-weight: 600;
+    line-height: 24px; /* 150% */
+    margin-bottom: 14px;
+
+    input {
+      outline: none;
+      border: none;
+      width: 21px;
+      height: 24px;
+      display: flex;
+      padding: 0px 4px;
+      align-items: flex-start;
+      border-radius: 4px;
+      background: var(--light, #f3efff);
+
+      font-size: 16px;
+      font-style: normal;
+      font-weight: 600;
+      line-height: 24px; /* 150% */
+    }
+  }
 `;
 const Input = styled.input`
   display: flex;
@@ -171,25 +347,35 @@ const Footer = styled.div`
   background: var(--main, #410fd4);
   display: flex;
   justify-content: space-between;
-  padding: 20px 24px;
+  padding: 20px 20px;
 
   .container {
     display: flex;
   }
 
-  .date {
-    display: inline-flex;
-    padding: 8px;
-    align-items: flex-start;
-    border-radius: 4px;
-    height: 24px;
-    background: var(--light, #f3efff);
-
-    color: var(--black, #09090a);
-
+  .join-inputs {
+    color: var(--white, #09090a);
     font-size: 16px;
     font-weight: 600;
     line-height: 24px; /* 150% */
+    display: flex;
+    gap: 4px;
+    padding-top: 8px;
+    input {
+      outline: none;
+      border: none;
+      width: 21px;
+      height: 24px;
+      display: flex;
+      padding: 0px 4px;
+      align-items: flex-start;
+      border-radius: 4px;
+
+      font-size: 16px;
+      font-style: normal;
+      font-weight: 600;
+      line-height: 24px; /* 150% */
+    }
   }
 
   .text {
