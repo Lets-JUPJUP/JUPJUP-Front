d7274a90d6279d203e1fd8d5c7a099ebbdf08409
wowalswjd <alswjd5491@naver.com>
2023-11-22 01:42:59 +0900

    :sparkles: 플로깅 참여한 멤버 조회하기 api 연결

---
+++ src/api/postjoin.js
@@ -0,0 +1,11 @@
+import { client } from "./client";
+
+// 플로깅 참여한 멤버 조회하기
+export const getPostsJoinMembers = async (id) => {
+  try {
+    const res = await client.get(`/api/v1/posts/${id}/members`);
+    return res.data;
+  } catch (err) {
+    throw err;
+  }
+};
