72931583c32a6adad9f4ba57c4674087fd808fe1
jimin song <songing01@gmail.com>
2023-11-23 10:42:00 +0900

    ✨ 공식행사 상세정보 조회 api 연결

--- src/api/event.js
+++ src/api/event.js
@@ -3,3 +3,7 @@ import { client } from "./client";
 export const eventGetEventList = async () => {
   return client.get("api/v1/eventInfos/lists");
 };
+
+export const eventGetEventDetail = async (id) => {
+  return client.get(`api/v1/eventInfos/${id}`);
+};
--- src/pages/Event/EventPage.js
+++ src/pages/Event/EventPage.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { styled } from "styled-components";
 import Header from "../../components/common/Header";
 import ic_star_default from "../../assets/common/ic_star_default.png";
@@ -6,18 +6,34 @@ import ic_star_clicked from "../../assets/common/ic_star_clicked.png";
 import AdBanner from "../../components/common/AdBanner";
 import CommentBox from "../../components/common/CommentBox";
 import CommentInput from "../../components/common/CommentInput";
-
+import { useParams } from "react-router-dom";
+import { eventGetEventDetail } from "../../api/event";
 const EventPage = () => {
   // 클릭 여부
   const [isClicked, setIsClicked] = useState(false);
+  const [data, setData] = useState({});
   const onStarClick = () => {
     setIsClicked(!isClicked);
   };
+  const { id } = useParams();
+  const getData = async () => {
+    try {
+      const res = (await eventGetEventDetail(id)).data.data;
+      setData(res);
+    } catch (err) {
+      alert("데이터를 가져올 수 없습니다.");
+    }
+  };
+
+  useEffect(() => {
+    getData();
+  }, []);
+
   return (
     <Wrapper>
       <Header title="공식 행사" />
       <div>
-        <Card />
+        <Card src={data.imageUrl}></Card>
         <div className="cardInfo">
           <div className="interest">
             <img
@@ -28,20 +44,28 @@ const EventPage = () => {
             />
             <BoldText>관심 있어요</BoldText>
           </div>
-          <BoldText className="link">홈페이지 &gt;</BoldText>
+          <BoldText
+            className="link"
+            onClick={() => {
+              window.open(data.infoUrl);
+            }}
+          >
+            홈페이지 &gt;
+          </BoldText>
         </div>
       </div>
       <AdBanner isNotFixed={true} />
 
-      <CommentDiv>
+      {/* 여기서 undefined 오류나서 주석처리 해뒀습니다 */}
+      {/* <CommentDiv>
         <CommentBox />
         <CommentBox />
         <CommentBox />
         <CommentBox />
         <CommentBox />
-      </CommentDiv>
+      </CommentDiv> */}
 
-      <CommentInput />
+      {/* <CommentInput /> */}
     </Wrapper>
   );
 };
@@ -77,7 +101,7 @@ const Wrapper = styled.div`
   }
 `;
 
-const Card = styled.div`
+const Card = styled.img`
   width: 280px;
   height: 376px;
   background: var(--grey, #e8e8e8);
