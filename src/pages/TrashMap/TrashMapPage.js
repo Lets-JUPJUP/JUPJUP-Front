a5d7856f0bf3da9b487bef9716414c9cc9e9ef8a
wowalswjd <alswjd5491@naver.com>
2023-11-24 06:34:22 +0900

    :sparkles: 반경 내 쓰레기통 조회 api 연결

--- src/pages/TrashMap/TrashMapPage.js
+++ src/pages/TrashMap/TrashMapPage.js
@@ -1,12 +1,26 @@
+import { useState } from "react";
 import Header from "../../components/common/Header";
 import KakaoMap from "../../components/trashmap/KakaoMap";
 import AdBanner from "../../components/common/AdBanner";
 
 const TrashMapPage = () => {
+  // 사용자의 현재 위치
+  const [curLocation, setCurLocation] = useState(null);
+
+  // 반경 내 쓰레기통 조회 api로 얻은 데이터
+  const [trashPageData, setTrashPageData] = useState([]);
+
+  // 사용자가 선택한 쓰레기통 정보
+  const [selectedData, setSelectedData] = useState(null);
   return (
     <>
       <Header title="성동구 쓰레기통 지도" />
-      <KakaoMap />
+      <KakaoMap
+        curLocation={curLocation}
+        setCurLocation={setCurLocation}
+        trashPageData={trashPageData}
+        setTrashPageData={setTrashPageData}
+      />
       <AdBanner />
     </>
   );
