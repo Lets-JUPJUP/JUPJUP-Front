3715cabc52eeef63f411d85016b872b3e910308d
jimin song <songing01@gmail.com>
2023-11-23 21:27:23 +0900

    🐛 첫 로그인시 데이터 겟 에러 안나도록 새로고침 순서 변경

--- src/pages/Login/LoginSettingsPage.js
+++ src/pages/Login/LoginSettingsPage.js
@@ -84,7 +84,7 @@ const LoginSettingsPage = () => {
           //SSE 구독 요청
           notificationSubscribeSSE(tempToken);
           alert("회원가입 완료");
-
+          window.location.reload();
           navigate("/");
         }
       } catch (err) {
