a3ed427c5a442201dcec863692982b756414b8a8
wowalswjd <alswjd5491@naver.com>
2023-11-24 15:28:54 +0900

    :lipstick: 현재 위치 마커 이미지, 각 쓰레기통 아이콘 별 바텀시트 추가

--- src/pages/TrashMap/TrashMapPage.js
+++ src/pages/TrashMap/TrashMapPage.js
@@ -1,7 +1,13 @@
 import { useState } from "react";
+import styled from "styled-components";
 import Header from "../../components/common/Header";
 import KakaoMap from "../../components/trashmap/KakaoMap";
 import AdBanner from "../../components/common/AdBanner";
+
+import { BottomSheet } from "react-spring-bottom-sheet";
+import "react-spring-bottom-sheet/dist/style.css";
+
+import TrashBottomSheet from "../../components/trashmap/TrashBottomSheet";
 
 const TrashMapPage = () => {
   // 사용자의 현재 위치
@@ -12,6 +18,15 @@ const TrashMapPage = () => {
 
   // 사용자가 선택한 쓰레기통 정보
   const [selectedData, setSelectedData] = useState(null);
+
+  // BottomSheet open 여부
+  const [bsOpen, setBsOpen] = useState(false);
+
+  // bottomsheet close하기
+  const onDisMiss = () => {
+    setBsOpen(false);
+  };
+
   return (
     <>
       <Header title="성동구 쓰레기통 지도" />
@@ -20,10 +35,21 @@ const TrashMapPage = () => {
         setCurLocation={setCurLocation}
         trashPageData={trashPageData}
         setTrashPageData={setTrashPageData}
+        setBsOpen={setBsOpen}
+        setSelectedData={setSelectedData}
       />
+      <StyledBottomSheet open={bsOpen} onDismiss={onDisMiss}>
+        <TrashBottomSheet selectedData={selectedData}/>
+      </StyledBottomSheet>
       <AdBanner />
     </>
   );
 };
 
 export default TrashMapPage;
+
+const StyledBottomSheet = styled(BottomSheet)`
+  /* [data-rsbs-header] {
+    background-color: var(--main);
+  } */
+`;
