ed2af664b26bf22b41c0d43ca1198899abaa22a1
wowalswjd <alswjd5491@naver.com>
2024-02-06 18:04:35 +0900

    :art: 비로그인 사용자 쓰레기통 지도 피드백 기능 접근 제한

--- src/components/trashmap/TrashBottomSheet.js
+++ src/components/trashmap/TrashBottomSheet.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 import { styled } from "styled-components";
 import { goodFeedbackList, badFeedbackList } from "./feedbackList";
 import { getTrashCanFeedback, postTrashCanFeedback } from "../../api/trashmap";
@@ -10,6 +11,10 @@ const TrashBottomSheet = ({ selectedData }) => {
     "https://mediahub.seoul.go.kr/uploads/mediahub/2021/09/tpuykXsdsBZQXeTWToBZZEYuYyQSMNof.jpeg";
   const recycleStationImgUrl =
     "https://blog.kakaocdn.net/dn/W21sn/btriHBWbViQ/L13mvEr72tjXcnfFi2S9Ak/img.jpg";
+
+  const isLogin = !!localStorage.getItem("juptoken"); // 로그인 여부
+
+  const navigate = useNavigate();
 
   // 각 쓰레기통에 대한 피드백 posted 여부
   const [isFeedbackPosted, setIsFeedbackPosted] = useState({
@@ -60,6 +65,11 @@ const TrashBottomSheet = ({ selectedData }) => {
     alert("이미 관리자에게 전달된 피드백입니다!");
   };
 
+  // 로그인하러가기 버튼 클릭 시 실행되는 함수
+  const onGoToLoginBtnClick = () => {
+    navigate("/login");
+  };
+
   return (
     <Wrapper>
       <div className="title">
@@ -76,47 +86,60 @@ const TrashBottomSheet = ({ selectedData }) => {
             : trashCanImgUrl
         }
       />
-      <div className="feedbackSection">
-        <div className="subTitle">👍 좋아요</div>
-        <div className="feedbackList">
-          {goodFeedbackList.map((feedback, index) => {
-            // 이미 제출한 피드백의 경우 보라색 버튼으로 변경
-            return isFeedbackPosted[feedback.feedbackCode] === false ? (
-              <TrashFeedBack
-                key={index}
-                onClick={() => onFeedbackClick(feedback.feedbackCode)}
-              >
-                {feedback.title}
-              </TrashFeedBack>
-            ) : (
-              <TrashFeedBackClicked key={index} onClick={onAlreadyClick}>
-                {feedback.title}
-              </TrashFeedBackClicked>
-            );
-          })}
+      {/* 로그인된 사용자가 아닐 경우 피드백 조회/전송 불가 */}
+      {isLogin ? (
+        <>
+          <div className="feedbackSection">
+            <div className="subTitle">👍 좋아요</div>
+            <div className="feedbackList">
+              {goodFeedbackList.map((feedback, index) => {
+                // 이미 제출한 피드백의 경우 보라색 버튼으로 변경
+                return isFeedbackPosted[feedback.feedbackCode] === false ? (
+                  <TrashFeedBack
+                    key={index}
+                    onClick={() => onFeedbackClick(feedback.feedbackCode)}
+                  >
+                    {feedback.title}
+                  </TrashFeedBack>
+                ) : (
+                  <TrashFeedBackClicked key={index} onClick={onAlreadyClick}>
+                    {feedback.title}
+                  </TrashFeedBackClicked>
+                );
+              })}
+            </div>
+          </div>
+          <DivisionLine />
+          <div className="feedbackSection">
+            <div className="subTitle">👎 나빠요</div>
+            <div className="feedbackList">
+              {badFeedbackList.map((feedback, index) => {
+                // 이미 제출한 피드백의 경우 보라색 버튼으로 변경
+                return isFeedbackPosted[feedback.feedbackCode] === false ? (
+                  <TrashFeedBack
+                    key={index}
+                    onClick={() => onFeedbackClick(feedback.feedbackCode)}
+                  >
+                    {feedback.title}
+                  </TrashFeedBack>
+                ) : (
+                  <TrashFeedBackClicked key={index} onClick={onAlreadyClick}>
+                    {feedback.title}
+                  </TrashFeedBackClicked>
+                );
+              })}
+            </div>
+          </div>
+        </>
+      ) : (
+        <div className="loginSection">
+          <div className="message">쓰레기통 피드백 기능은</div>
+          <div className="message">로그인 후 이용 가능합니다.</div>
+          <GoToLoginBtn onClick={onGoToLoginBtnClick}>
+            로그인하러 가기
+          </GoToLoginBtn>
         </div>
-      </div>
-      <DivisionLine />
-      <div className="feedbackSection">
-        <div className="subTitle">👎 나빠요</div>
-        <div className="feedbackList">
-          {badFeedbackList.map((feedback, index) => {
-            // 이미 제출한 피드백의 경우 보라색 버튼으로 변경
-            return isFeedbackPosted[feedback.feedbackCode] === false ? (
-              <TrashFeedBack
-                key={index}
-                onClick={() => onFeedbackClick(feedback.feedbackCode)}
-              >
-                {feedback.title}
-              </TrashFeedBack>
-            ) : (
-              <TrashFeedBackClicked key={index} onClick={onAlreadyClick}>
-                {feedback.title}
-              </TrashFeedBackClicked>
-            );
-          })}
-        </div>
-      </div>
+      )}
     </Wrapper>
   );
 };
@@ -159,6 +182,19 @@ const Wrapper = styled.div`
       margin-top: 12px;
     }
   }
+
+  .loginSection {
+    height: 270px;
+
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+
+    .message {
+      font-size: 16px;
+    }
+  }
 `;
 
 const TrashImage = styled.div`
@@ -192,4 +228,17 @@ const DivisionLine = styled.div`
   width: 95%;
   height: 1.2px;
   background: var(--main, "#410FD4");
+`;
+
+const GoToLoginBtn = styled.button`
+  margin-top: 16px;
+  padding: 8px 12px;
+  border: 0px;
+  border-radius: 4px;
+
+  font-size: 16px;
+  font-weight: 600;
+
+  background: var(--sub, #beef62);
+  color: var(--main, #410fd4);
 `;
